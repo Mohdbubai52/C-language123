dc530bc5c957e4e943703332ac8b318d135f4c5d
Mohdbubai52 <113508573+Mohdbubai52@users.noreply.github.com>
2022-10-03 00:19:05 +0530

    flutter_assigment..

---
+++ #1_display_name.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("My name is Mohammed bubai...:\n");
+	printf("My birth date is 12/12/2003...\n");
+	printf("My age is 18 years..:\n");
+	printf("I live in banswara rajsthan...\n");
+	return 0;
+}
---
+++ #2_add_sub_multi_div.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	
+	printf("Enter first number.....",num1);
+	scanf("%i",&num1);
+	
+	printf("\nEnter second number....",num2);
+	scanf("%i",&num2);
+	
+	printf("addition subraction divion and multiplication of give number is\n");
+	printf("\naddition of first number %i and second number %i is %i.....",num1,num2,num1+num1);
+	printf("\nsubraction of first number %i and second number %i is %i.....",num1,num2,num1-num1);
+	printf("\nmultiplication of first number %i and second number %i is %i.....",num1,num2,num1*num1);
+	printf("\ndivision of first number %i and second number %i is %i.....",num1,num2,num1/num1);
+	
+}
